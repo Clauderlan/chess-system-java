e1b1cabc6417a81cbfd3a75df4df1593d2ff8217
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-03-25 20:05:48 -0300

    Add files via upload

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Board;
+import chess.ChessMatch;
+import chess.ChessPiece;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
