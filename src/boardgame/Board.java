a903a2068c0cf314ea74b0bb2369ae9982399f9c
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-03-26 11:17:49 -0300

    Error solved

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -14,11 +14,15 @@ public class Board {
     }
 
     public Piece piece(int row, int column){
-        if(!positionExists(row, column)) throw new BoardException("Position not on the board.");
+        if(!positionExists(row, column)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[row][column];
     }
     public Piece piece(Position position){
-        if(!positionExists(position)) throw new BoardException("Position not on the board.");
+        if(!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
     public void placePiece(Piece piece, Position position){
@@ -27,13 +31,15 @@ public class Board {
         piece.position = position;
     }
     private boolean positionExists(int row, int column){
-        return row>=0 && row < rows && column >= 0 && column < columns;
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
     public boolean positionExists(Position position){
         return positionExists(position.getRow(), position.getColumn());
     }
     public boolean thereIsAPiece(Position position){
-        if(!positionExists(position)) throw new BoardException("Position not on the board.");
+        if(!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
         return piece(position) != null;
     }
 
