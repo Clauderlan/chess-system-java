d7a92e5e8e6b9478be860c61282d47956792b5d4
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-03-28 22:15:30 -0300

    Printing board King

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,32 +1,34 @@
 package boardgame;
 
 public abstract class Piece {
-    protected Position position;
-    private Board board;
 
-    public Piece(Board board){
-        this.board = board;
-        position = null;
-    }
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
-    public abstract boolean[][] possibleMoves();
-
-    public boolean possibleMove(Position position) {
-        return possibleMoves()[position.getColumn()][position.getColumn()];
-    }
-
-    public boolean isThereAnyPossibleMove(){
-        boolean[][] mat = possibleMoves();
-        for(int x =0; x<mat.length; x++){
-            for (int y = 0; y < mat.length; y++){
-                if(mat[x][y]) return true;
-
-            }
-        }
-        return false;
-    }
-
-    protected Board getBoard(){
-        return this.board;
-    }
+	protected Board getBoard() {
+		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
